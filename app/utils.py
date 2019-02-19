b12c68a45e0d9afa664797d9f27de84e6b7a33f0
Cédric Tarbouriech <cedtrabou@gmail.com>
2019-02-20 00:05:12 +0100

    format: Reformat code.

--- app/utils.py
+++ app/utils.py
@@ -9,11 +9,13 @@ class Date:
         self.__month = month
         self.__year = year
 
+
 class Place:
     def __init__(self, label, x, y):
         self.__label = label
         self.__x = x
         self.__y = y
+
 
 class Event:
     def __init__(self, date: Date, place: Place):
